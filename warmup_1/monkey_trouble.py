eae73ad2ae0ea4e90050158c048ce9f1beda3a5b
Arthur Nobrega <arthurthiago@gmail.com>
2016-02-03 21:46:53 -0200

    Developing monkey_trouble http://codingbat.com/prob/p120546

---
+++ warmup_1/monkey_trouble.py
@@ -0,0 +1,22 @@
+"""
+http://codingbat.com/prob/p120546
+
+We have two monkeys, a and b, and the parameters a_smile and b_smile indicate
+if each is smiling. We are in trouble if they are both smiling or if neither
+of them is smiling. Return True if we are in trouble.
+
+monkey_trouble(True, True) → True
+monkey_trouble(False, False) → True
+monkey_trouble(True, False) → False
+"""
+
+
+def monkey_trouble(a_smile, b_smile):
+    return a_smile == b_smile
+
+
+if __name__ == '__main__':
+    assert monkey_trouble(True, True) == True
+    assert monkey_trouble(False, False) == True
+    assert monkey_trouble(True, False) == False
+    assert monkey_trouble(False, True) == False
